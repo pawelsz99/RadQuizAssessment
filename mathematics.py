6eee0e999aa2119d70788e6a49f33ecf0d5bf22c
Pawel <52848605+pawelsz99@users.noreply.github.com>
2021-03-17 15:57:34 +0100

    Added Q and correct A for math

--- mathematics.py
+++ mathematics.py
@@ -12,26 +12,28 @@ from guizero import App, Text, PushButton, Box, Window
 from question import Question
 
 list_questions = {
-    1: Question("Text Q1", "Text q1a1", "Text q1a2", "Text q1a3", "Text q1a4"),
-    2: Question("Text Q2", "Text q2a1", "Text q2a2", "Text q2a3", "Text q2a4"),
-    3: Question("Text Q3", "Text q3a1", "Text q3a2", "Text q3a3", "Text q3a4"),
-    4: Question("Text Q4", "Text q4a1", "Text q4a2", "Text q4a3", "Text q4a4"),
-    5: Question("Text Q5", "Text q5a1", "Text q5a2", "Text q5a3", "Text q5a4"),
-    6: Question("Text Q6", "Text q6a1", "Text q6a2", "Text q6a3", "Text q6a4"),
-    7: Question("Text Q7", "Text q7a1", "Text q7a2", "Text q7a3", "Text q7a4"),
-    8: Question("Text Q8", "Text q8a1", "Text q8a2", "Text q8a3", "Text q8a4"),
-    9: Question("Text Q9", "Text q9a1", "Text q9a2", "Text q9a3", "Text q9a4"),
-    10: Question("Text Q10", "Text q10a1", "Text q10a2", "Text q10a3", "Text q10a4")
+    1: Question("An empty container weighs 120g. When 50 lollipops were put in it the weight was 870g. What is the weight of one lollipop?", "15 Grams", "Text q1a2", "Text q1a3", "Text q1a4"),
+    2: Question("Anne is going to Malta. How many euros will she get for £150 when the exchange rate is 1.18 euros to a pound?", "177 euros ", "Text q2a2", "Text q2a3", "Text q2a4"),
+    3: Question("A car travels at a constant speed of 63 mph for 20 minutes. How far does the car travel in this time? (pic)", "21 miles ", "Text q3a2", "Text q3a3", "Text q3a4"),
+    4: Question("A liquid is warmed from –3C to +5C. By how many degrees has its temperature risen?", "By 8 ", "Text q4a2", "Text q4a3", "Text q4a4"),
+    5: Question("Some water has been added to this measuring jar. How much more water is needed to fill the jar to 1.5 litres? (pic)", "0.6 litres", "Text q5a2", "Text q5a3", "Text q5a4"),
+    6: Question("The number of pupils in each year group in a secondary school was recorded and this pie chart drawn. There are 1200 pupils in the school. How many pupils were there in S5/6? (pic)", "200 pupils", "Text q6a2", "Text q6a3", "Text q6a4"),
+    7: Question("The number of people using a gym each day was recorded for a week and this compound bar chart was drawn. Who has used the gym the most? Male of Female? (pic)", "Female ", "Text q7a2", "Text q7a3", "Text q7a4"),
+    8: Question("Sally scored the following marks in three of her tests. Maths: 25 out of 40 English: 32 out of 50 Science: 38 out of 60 In which subject did she do best in?", "English ", "Text q8a2", "Text q8a3", "Text q8a4"),
+    9: Question("Calculate the perimeter of the shape. (pic)", "52 cm", "Text q9a2", "Text q9a3", "Text q9a4"),
+    10: Question("Increase £80 by 60%", "128£", "Text q10a2", "Text q10a3", "Text q10a4")
 }
 
 questions_order = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-counter = 0
+order_counter = 0
 # this will be used in randomizing questions order
 
 
 class Mathematics:
     def __init__(self, app):
         app = app
+        global order_counter
+        order_counter = 0
         #------------------------------Mathematics Window-----------------------------------#
 
         math_window = Window(app, width=700, height=700, bg="#FFED7C")
@@ -150,12 +152,12 @@ class Mathematics:
         self.next_question()
 
     def next_question(self):
-        global counter
-        q = list_questions[questions_order[counter]].get_q_text()
-        a1 = list_questions[questions_order[counter]].get_a1_text()
-        a2 = list_questions[questions_order[counter]].get_a2_text()
-        a3 = list_questions[questions_order[counter]].get_a3_text()
-        a4 = list_questions[questions_order[counter]].get_a4_text()
+        global order_counter
+        q = list_questions[questions_order[order_counter]].get_q_text()
+        a1 = list_questions[questions_order[order_counter]].get_a1_text()
+        a2 = list_questions[questions_order[order_counter]].get_a2_text()
+        a3 = list_questions[questions_order[order_counter]].get_a3_text()
+        a4 = list_questions[questions_order[order_counter]].get_a4_text()
 
         self.math_question.value = q
 
@@ -164,4 +166,4 @@ class Mathematics:
         self.math_answer_3.text = a3
         self.math_answer_4.text = a4
 
-        counter += 1
+        order_counter += 1
